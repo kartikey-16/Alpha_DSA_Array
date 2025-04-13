b9b6639d8e140a55bbf59bea1955cea600a08de1
Kumar Kartikey Choudhary <choudharykartik733@gmail.com>
2025-04-13 13:59:48 +0530

    Add files via upload

---
+++ Stock.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class Stock {
+	
+	private static int buyAndSellStock(int[] prices) {
+		int buyPrice = Integer.MAX_VALUE;
+		int maxProfit = 0;
+		
+		for(int i = 0 ; i < prices.length ; i++)
+		{
+			if(buyPrice < prices[i])
+			{
+				int profit = prices[i] - buyPrice;
+				maxProfit = Math.max(maxProfit, buyPrice);
+			}
+			else
+			{
+				buyPrice = prices[i];
+			}
+		}
+		return maxProfit;
+		
+	}
+	
+
+	public static void main(String args[])
+	{
+		int prices[] = {7,1,5,3,6,4};
+		System.out.println(buyAndSellStock(prices));
+		
+	}
+
+	
+}
